05de776195fbb0e3bc42c8b2a09f7520af4c8b36
Jake Joseph <jakejosephcs@gmail.com>
2020-08-07 12:48:05 -0400

    Finish header and myplaces

---
+++ frontend/src/places/pages/UserPlaces.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import PlaceList from '../components/PlaceList';
+
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'CN Tower',
+    description: 'In the 6ix',
+    imageUrl:
+      'https://media.blogto.com/articles/2020624-cn-tower-toronto-2.jpg?w=2048&cmd=resize_then_crop&height=1365&quality=70',
+    address: '290 Bremner Blvd, Toronto, ON M5V 3L9',
+    location: {
+      lat: 43.6425662,
+      lng: -79.3892508,
+    },
+    creator: 'u1',
+  },
+  {
+    id: 'p2',
+    title: 'CN Tower',
+    description: 'In the 6ix',
+    imageUrl:
+      'https://media.blogto.com/articles/2020624-cn-tower-toronto-2.jpg?w=2048&cmd=resize_then_crop&height=1365&quality=70',
+    address: '290 Bremner Blvd, Toronto, ON M5V 3L9',
+    location: {
+      lat: 43.6425662,
+      lng: -79.3892508,
+    },
+    creator: 'u2',
+  },
+];
+
+function UserPlaces() {
+  return <PlaceList items={DUMMY_PLACES} />;
+}
+
+export default UserPlaces;
