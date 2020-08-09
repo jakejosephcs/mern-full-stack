1a7a9c93fbaf11904d6321c3a7cccfc38a59d46d
Jake Joseph <jakejosephcs@gmail.com>
2020-08-09 17:27:49 -0400

    Add functionality for fetching coordinates

--- backend/controllers/places-controller.js
+++ backend/controllers/places-controller.js
@@ -3,6 +3,7 @@ const { v4: uuidv4 } = require('uuid');
 const { validationResult } = require('express-validator');
 
 const HttpError = require('../models/http-error');
+const getCoordsForAddress = require('../util/location.js');
 
 let DUMMY_PLACES = [
   {
@@ -52,15 +53,23 @@ const getPlacesByUserId = (req, res, next) => {
   });
 };
 
-const createPlace = (req, res, next) => {
+const createPlace = async (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    console.log(errors);
-    throw new HttpError('Invalid Inputs passed, please check your data', 422);
+    return next(
+      new HttpError('Invalid Inputs passed, please check your data', 422)
+    );
   }
 
-  const { title, description, coordinates, address, creator } = req.body;
+  const { title, description, address, creator } = req.body;
+
+  let coordinates;
+  try {
+    coordinates = await getCoordsForAddress(address);
+  } catch (error) {
+    return next(error);
+  }
 
   const createdPlace = {
     id: uuidv4(),
